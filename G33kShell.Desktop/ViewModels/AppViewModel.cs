c20f2b767edf2cb698eb86e934ce69097d28e76b
Dean Edis <dean.edis@gmail.com>
2024-09-07 19:31:39 +0100

    Other: G33kShell solution added.

---
+++ G33kShell.Desktop/ViewModels/AppViewModel.cs
@@ -0,0 +1,38 @@
+// Code authored by Dean Edis (DeanTheCoder).
+// Anyone is free to copy, modify, use, compile, or distribute this software,
+// either in source code form or as a compiled binary, for any non-commercial
+// purpose.
+//
+// If you modify the code, please retain this copyright header,
+// and consider contributing back to the repository or letting us know
+// about your modifications. Your contributions are valued!
+//
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND.
+using System.Windows.Input;
+using Avalonia;
+using CSharp.Core.Commands;
+using CSharp.Core.Extensions;
+using CSharp.Core.ViewModels;
+using G33kShell.Desktop.Views;
+
+namespace G33kShell.Desktop.ViewModels;
+
+public class AppViewModel : ViewModelBase
+{
+    public ICommand AboutCommand { get; }
+
+    public AppViewModel()
+    {
+        var isOpen = false;
+        AboutCommand = new RelayCommand(
+            _ =>
+            {
+                if (isOpen)
+                    return;
+                var dialog = new AboutDialog();
+                dialog.Opened += (_, _) => isOpen = true;
+                dialog.Closed += (_, _) => isOpen = false;
+                dialog.ShowDialog(Application.Current?.GetMainWindow());
+            });
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TerminalFX
+[![Twitter Follow](https://img.shields.io/twitter/follow/deanthecoder?style=social)](https://twitter.com/deanthecoder)
+
+# G33k Shell - Retro Terminal
