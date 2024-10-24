4509ee99d6f44f2dd7c6222203f5ca071fcc9b00
Dean Edis <dean.edis@gmail.com>
2024-10-24 14:07:09 +0100

    Other: Add 'del' as an alias for 'rm'.

--- G33kShell.Desktop/Terminal/Commands/MyCommandType.cs
+++ G33kShell.Desktop/Terminal/Commands/MyCommandType.cs
@@ -60,7 +60,7 @@ public enum MyCommandType
     [Command(typeof(RevealCommand), LongName = "reveal", Description = "View the target in the OS file browser.")]
     Reveal,
     
-    [Command(typeof(RmCommand), LongName = "rm", Description = "Recursively delete a folder or file(s).")]
+    [Command(typeof(RmCommand), LongName = "rm", ShortName = "del", Description = "Recursively delete a folder or file(s).")]
     Rm,
     
     [Command(typeof(ScreensaverCommand), LongName = "screensaver", Description = "Select and activate the screensaver.")]
