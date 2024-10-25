19daee9c7b4c9b29eda77cfc5249b073e98f49e4
Dean Edis <dean.edis@gmail.com>
2024-10-25 12:42:05 +0100

    Feature: Added directory sync option in CopyCommand.

--- G33kShell.Desktop/Terminal/Commands/CopyCommand.cs
+++ G33kShell.Desktop/Terminal/Commands/CopyCommand.cs
@@ -19,16 +19,18 @@ using NClap.Metadata;
 
 namespace G33kShell.Desktop.Terminal.Commands;
 
-[CommandDescription("Copy files and directories.")]
+[CommandDescription(
+    "Copy files and directories.",
+    "If the source and target are directories the 'sync' option can be used to ensure the contents match.")]
 public class CopyCommand : CommandBase
 {
     [PositionalArgument(ArgumentFlags.Multiple, Description = "One or more pairs of source/destinations.")]
     [UsedImplicitly]
     public string[] Paths { get; set; }
 
-    // [NamedArgument(Description = "Display in bare format.", ShortName = "b")]
-    // [UsedImplicitly]
-    // public bool BareFormat { get; set; }
+    [NamedArgument(Description = "Sync the content of source and target directories.", LongName = "sync")]
+    [UsedImplicitly]
+    public bool Sync { get; set; }
 
     protected override async Task<bool> Run(ITerminalState state)
     {
@@ -37,47 +39,95 @@ public class CopyCommand : CommandBase
             WriteLine("One or more pairs of source/destinations must be specified.");
             return false;
         }
-        
+
         for (var i = 0; i < Paths.Length; i += 2)
         {
             var source = state.CurrentDirectory.Resolve(Paths[i]);
             var dest = state.CurrentDirectory.Resolve(Paths[i + 1]);
-
-            // Copy directory to under another directory.
-            var sourceDir = source.Contains('*') ? source.ToFile().Directory : source.ToDir();
-            if (sourceDir == null)
-                continue;
-            
-            if (sourceDir.Exists && !source.Contains('*'))
+            try
             {
-                try
+                // Syncing two directories?
+                if (Sync && source.ToDir().Exists)
+                {
+                    await Task.Run(() => RunSync(source, dest));
+                    continue;
+                }
+
+                // Copy directory to under another directory.
+                var sourceDir = source.Contains('*') ? source.ToFile().Directory : source.ToDir();
+                if (sourceDir == null)
+                    continue;
+
+                if (sourceDir.Exists && !source.Contains('*'))
                 {
                     await Task.Run(() => sourceDir.CopyTo(dest.ToDir()));
                     continue;
                 }
-                catch (Exception ex)
-                {
-                    WriteLine($"An error occurred: {ex.Message}");
-                    return false;
-                }
-            }
 
-            // Copy files to a directory?
-            var sourceInfos = source.Contains('*') ? sourceDir.GetFileSystemInfos(source.ToFile().Name) : [source.ToFile()];
-            if (sourceInfos.Length == 0)
-                continue; // Nothing to do.
-            var isTargetADir = dest.ToDir().Exists || dest.EndsWith('/') || dest.EndsWith('\\');
-            if (isTargetADir)
-            {
-                foreach (var sourceFile in sourceInfos)
-                    sourceFile.CopyTo(dest.ToDir());
-                continue;
+                // Copy files to a directory?
+                var sourceInfos = source.Contains('*') ? sourceDir.GetFileSystemInfos(source.ToFile().Name) : [source.ToFile()];
+                if (sourceInfos.Length == 0)
+                    continue; // Nothing to do.
+                var isTargetADir = dest.ToDir().Exists || dest.EndsWith('/') || dest.EndsWith('\\');
+                if (isTargetADir)
+                {
+                    foreach (var sourceFile in sourceInfos)
+                        sourceFile.CopyTo(dest.ToDir(), true);
+                    continue;
+                }
+
+                // Copy file(s) to a file. (I.e. Copy with new name)
+                (sourceInfos.Last() as FileInfo)?.CopyTo(dest.ToFile(), true);
             }
-            
-            // Copy file(s) to a file. (I.e. Copy with new name)
-            (sourceInfos.Last() as FileInfo)?.CopyTo(dest.ToFile());
+            catch (Exception ex)
+            {
+                WriteLine($"An error occurred copying {Paths[i]} to {Paths[i + 1]}: {ex.Message}");
+                return false;
+            }
         }
-        
+
         return true;
+    }
+
+    private static void RunSync(string source, string dest)
+    {
+        var sourceDir = source.ToDir();
+        var destDir = dest.ToDir();
+
+        if (!destDir.Exists)
+            destDir.Create();
+
+        // Normalizing the paths by making them relative.
+        var allSources = sourceDir
+            .EnumerateFileSystemInfos("*", SearchOption.AllDirectories)
+            .Select(s => new
+            {
+                RelativePath = Path.GetRelativePath(source, s.FullName), Info = s
+            })
+            .ToArray();
+
+        var allTargets = destDir
+            .EnumerateFileSystemInfos("*", SearchOption.AllDirectories)
+            .Select(t => new
+            {
+                RelativePath = Path.GetRelativePath(dest, t.FullName), Info = t
+            })
+            .ToArray();
+
+        // Finding excess items in target by comparing relative paths
+        var excessItems = allTargets
+            .Where(target => !allSources.Any(src => src.RelativePath.Equals(target.RelativePath, StringComparison.OrdinalIgnoreCase)))
+            .Select(t => t.Info)
+            .ToArray();
+
+        foreach (var excessItem in excessItems)
+            excessItem.TryDelete();
+
+        // Copy the content that does exist.
+        var fileSystemInfos = sourceDir
+            .GetFileSystemInfos()
+            .ToList();
+        fileSystemInfos
+            .ForEach(o => o.CopyTo(destDir, true));
     }
 }
