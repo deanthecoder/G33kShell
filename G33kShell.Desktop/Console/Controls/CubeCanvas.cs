1d702e91f634ee5ecc2313e32ea69f940a9f2c51
Dean Edis <dean.edis@gmail.com>
2025-02-16 15:39:26 +0000

    Feature: Add 3D 'cube' screensaver.

---
+++ G33kShell.Desktop/Console/Controls/CubeCanvas.cs
@@ -0,0 +1,62 @@
+// Code authored by Dean Edis (DeanTheCoder).
+// Anyone is free to copy, modify, use, compile, or distribute this software,
+// either in source code form or as a compiled binary, for any non-commercial
+//  purpose.
+// 
+// If you modify the code, please retain this copyright header,
+// and consider contributing back to the repository or letting us know
+// about your modifications. Your contributions are valued!
+// 
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND.
+using System;
+using System.Diagnostics;
+using System.Numerics;
+using G33kShell.Desktop.Console._3D;
+using JetBrains.Annotations;
+
+namespace G33kShell.Desktop.Console.Controls;
+
+/// <summary>
+/// A canvas to displaying an animated 3D scene.
+/// </summary>
+[DebuggerDisplay("CubeCanvas:{X},{Y} {Width}x{Height}")]
+[UsedImplicitly]
+public class CubesCanvas : ScreensaverBase
+{
+    private SceneBackground m_background;
+    private SceneObject m_cube;
+
+    public CubesCanvas(int width, int height) : base(width, height)
+    {
+        Name = "cube";
+    }
+
+    protected override void BuildScreen(ScreenData screen)
+    {
+        base.BuildScreen(screen);
+
+        m_background = new ChequeredSceneBackground(Foreground, Background, 0.08);
+        m_cube = new CubeObject(0.5f, [
+            new Attr('.', Foreground, Background),
+            new Attr(';', Foreground, Background),
+            new Attr('i', Foreground, Background),
+            new Attr('S', Foreground, Background),
+            new Attr('8', Foreground, Background),
+            new Attr('l', Foreground, Background)
+        ]);
+    }
+
+    protected override void UpdateFrame(ScreenData screen)
+    {
+        var time = 1.0f * FrameNumber / TargetFps;
+
+        // Prepare the scene.
+        m_cube.Rotation = new Vector3(1.8f, 0.3f, 0.7f) * time;
+        m_cube.Position = new Vector3(0, 0, 8.0f * (0.5f + 0.5f * MathF.Sin(time)));
+
+        // Draw the scene.
+        var scene = new Scene3D(screen, m_background);
+        scene.AddObject(m_cube);
+        scene.Render(time);
+    }
+}
